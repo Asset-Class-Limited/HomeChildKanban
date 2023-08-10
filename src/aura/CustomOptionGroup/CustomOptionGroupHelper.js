471c0f8c9ca2ee93eda36587c2bfd9605f61e47f
fischerp <>
2023-08-10 17:08:10 +0100

    strike_filterBy to strikeFilterBy

--- src/aura/CustomOptionGroup/CustomOptionGroupHelper.js
+++ src/aura/CustomOptionGroup/CustomOptionGroupHelper.js
@@ -14,8 +14,8 @@
         var searchTerm = event.getParam('arguments');
 
         body.forEach(function(child) {
-            if (child.strike_filterBy) {
-                child.strike_filterBy(searchTerm[0]);
+            if (child.strikeFilterBy) {
+                child.strikeFilterBy(searchTerm[0]);
                 component.set('v.hidden', component.get('v.hidden') && child.get('v.hidden'));
             } else {
                 helper.filterBy(component, event, helper, child);
